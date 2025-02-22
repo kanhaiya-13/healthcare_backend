13a9175f8cf097896ee457562dadc113768cc820
Kanhaiyalal Yadav <kanhaiya13004@gmail.com>
2025-02-22 18:55:32 +0530

    moved db credentials to .env

--- backend/db.js
+++ backend/db.js
@@ -3,11 +3,11 @@ require("dotenv").config();
 
 // Create a connection to the database
 const connection = mysql.createConnection({
-  host: "localhost", // XAMPP server is usually localhost
-  user: "root", // Default MySQL user
+  host: process.env.host, // XAMPP server is usually localhost
+  user: process.env.user, // Default MySQL user
   password: process.env.db_password, // Your MySQL root password (leave blank if none)
-  database: "healthcare", // Your database name
-  port: 3306, // Default MySQL port
+  database: process.env.database, // Your database name
+  port: process.env.port, // Default MySQL port
 });
 
 // Connect to the database
